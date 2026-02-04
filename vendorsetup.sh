467c32a02665fdea13c2553573235112a5ebffe4
SirRGB <sirrgb@proton.me>
2026-02-04 16:20:02 +0100

    feat: fetch apks conditionally

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,12 +3,15 @@
 # be strict on failures
 set -euo pipefail
 
-echo "vendor/partner_gms/vendorsetup.sh called"
+echo "Fetching microg files"
 
 get_microg_files() {
     local trg_name src_name src_url
     trg_name="${1}"
     src_name="${2}"
+    # Check for file
+    [[ -f "$(dirname "${BASH_SOURCE[0]}")"/"${trg_name}"/"${trg_name}".apk ]] && return
+    # Latest tag
     src_url=$(curl --fail --silent --show-error https://api.github.com/repos/microg/GmsCore/releases/latest | grep -E "/${src_name}-[0-9]+.apk\"" | cut -d"\"" -f4)
     curl --fail --silent --show-error --location "${src_url}" --output "$(dirname "${BASH_SOURCE[0]}")"/"${trg_name}"/"${trg_name}".apk
 }
@@ -17,8 +20,12 @@ get_fdroid_files() {
     local trg_name src_name
     trg_name="${1}"
     src_name="${2}"
+    [[ -f "$(dirname "${BASH_SOURCE[0]}")"/"${trg_name}"/"${trg_name}".apk ]] && return
     curl --fail --silent --show-error https://f-droid.org/"${src_name}".apk --output "$(dirname "${BASH_SOURCE[0]}")"/"${trg_name}"/"${trg_name}".apk
 }
+
+# Drop apks older than a day
+find "$(dirname "${BASH_SOURCE[0]}")"/ -name "*.apk" -ctime +1 -exec rm {} \;
 
 get_microg_files GmsCore "com.google.android.gms"
 get_microg_files FakeStore "com.android.vending"
